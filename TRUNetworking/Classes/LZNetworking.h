52c6fb843c3da8b06dcd146a513a04c6770af995
apple <153444073@qq.com>
2020-11-03 11:53:01 +0800

    init project

---
+++ TRUNetworking/Classes/LZNetworking.h
@@ -0,0 +1,20 @@
+//
+//  LZNetworking.h
+//  TRUNetworking_Example
+//
+//  Created by apple on 2020/11/3.
+//  Copyright © 2020 Butterffly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZNetworking : NSObject
+
+
++(void)runDoing;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/LZNetworking.m
@@ -0,0 +1,19 @@
+//
+//  LZNetworking.m
+//  TRUNetworking_Example
+//
+//  Created by apple on 2020/11/3.
+//  Copyright © 2020 Butterffly. All rights reserved.
+//
+
+#import "LZNetworking.h"
+
+@implementation LZNetworking
+
++(void)runDoing{
+    
+    NSLog(@"组件化练习");
+    
+}
+
+@end
